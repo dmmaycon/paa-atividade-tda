b9a7cc105a2b9bc847475acb0ffad1f46894623e
Abatti13 <colossi.gabriel@gmail.com>
2021-08-18 15:12:40 -0300

    Update TDA.C

--- TDA.C
+++ TDA.C
@@ -15,7 +15,16 @@ SalaAula *criaSala(int fileiras, int carteiras)
   SalaAula *disposicao = (SalaAula *) SalaAula(fileiras, carteiras);
 }
 
-
+/******************* função que mudar a posição do aluno *********************/
+boolean muda_posi(int fila_atual, int cart_atual, int fila_nova, int cart_nova){
+    char *POS_nome;
+    if (SalaAula[fila_nova][cart_nova] == POS_vazia){
+        POS_nome = &SalaAula[fila_atual][cart_atual]
+    }
+    else{
+        return false;
+    }
+}
 
 
 
