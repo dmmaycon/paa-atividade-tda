d5332a164bbe35903c3e275df232f2e97675ddff
dmmaycon <mayconmoraes.dm@gmail.com>
2021-08-18 11:35:59 -0300

    primeiras adiçoes no TDA.C

--- TDA.C
+++ TDA.C
@@ -1,1 +1,21 @@
 #include "TDA_PRIVADO.H"
+#include "string.h"
+
+typedef struct SalaAula
+{
+    int numeroFileira;
+    int carteiras;
+    // char disposicaoSala[][]; 
+    SalaAula (*criaSala) (int f, int c);
+
+} SalaAula;
+
+SalaAula *criaSala(int fileiras, int carteiras)
+{   
+  SalaAula *disposicao = (SalaAula *) SalaAula(fileiras, carteiras);
+}
+
+
+
+
+
