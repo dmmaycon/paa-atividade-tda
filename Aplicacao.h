20c842e377b5c17c3c61689574f23c0bd576b21e
Abatti13 <colossi.gabriel@gmail.com>
2021-08-30 23:37:39 -0300

    Update Aplicacao.h

--- Aplicacao.h
+++ Aplicacao.h
@@ -4,11 +4,48 @@
 #include <stdbool.h>
 #include <stddef.h>
 
+#ifndef APLICACAO_H
+#define APLICACAO_H
 
-struct {
+
+typedef struct {
     int info;
-} typedef ElementInt;
+} ItemInt;
 
-struct {
+typedef struct {
     char info[255];
-} typedef ElementChar;
+} ItemChar;
+
+void VeTopoInt(Pilha* pilha) {
+    printf("%d\n", ((ItemInt*)Top(pilha))->info);
+}
+
+void VeTopoChar(Pilha* pilha) {
+    printf("%s\n", ((ItemChar*)Top(pilha))->info);
+}
+
+void EsvaziaPilhaInt(Pilha* pilha) {
+    while(!IsEmpty(pilha)) {
+        printf("%d\n", ((ItemInt*)Pop(pilha))->info);
+    }
+}
+void EsvaziaPilhaChar(Pilha* pilha) {
+    while(!IsEmpty(pilha)) {
+        printf("%s\n",((ItemChar*)Pop(pilha))->info);
+    }
+}
+
+void addElementoInt(int v, Pilha* p) {
+    ItemInt* i = (ItemInt*) malloc(sizeof(ItemInt));
+    i->info = v;
+    //printf("%d\n", i->info);
+    Push(i, p);
+}
+void addElementoChar(char s[], Pilha* p) {
+    ItemChar* i = (ItemChar*) malloc(sizeof(ItemChar));
+    strcpy(i->info, s);
+    //printf("%s\n", s);
+    Push(i, p);
+}
+
+#endif
